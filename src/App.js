0ab91e05858f50a0229aa20fc9560d2ea52c4d92
thimerlan <thimerlan.web@gmail.com>
2022-11-11 18:17:45 +0300

    added some font-style function

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { GrammarlyEditorPlugin, Grammarly } from "@grammarly/editor-sdk-react";
 import "./App.css";
 
 function App() {
   const [char, setChar] = useState("");
-
+  const [changeFont, setChangeFont] = useState([]);
+  const [changeColor, setChangeColor] = useState(["#ffd100"]);
   let remainAll = 120;
   let remain = char.length - remainAll;
+  const txtFocus = useRef();
+  useEffect(() => {
+    txtFocus.current.focus();
+  }, [changeFont]);
 
   return (
     <div className="App">
+      <div className="btns">
+        <button
+          onClick={() => {
+            setChangeFont(changeFont !== "bold" ? "bold" : "");
+          }}
+          className={changeFont === "bold" ? "active-f bold" : "bold"}
+        >
+          B
+        </button>
+        <button
+          onClick={() => {
+            setChangeFont(changeFont !== "italic" ? "italic" : "");
+          }}
+          className={changeFont === "italic" ? "active-f italic" : "italic"}
+        >
+          I
+        </button>
+        <div className="changeColor">
+          <input
+            value={changeColor}
+            type="color"
+            onChange={(e) => {
+              setChangeColor(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+
       <Grammarly>
         <GrammarlyEditorPlugin
           contenteditable="true"
           clientId="client_HGu1p4bnjopAHi1vpAgB1x"
-          config={{ documentDialect: "british" }}
         >
           <textarea
+            style={{ color: changeColor }}
+            ref={txtFocus}
+            autoComplete="on"
             value={char}
             onChange={(e) => {
               setChar(e.target.value);
             }}
             maxLength={120}
+            id={
+              changeFont !== "italic"
+                ? changeFont === "bold"
+                  ? "txt-bold"
+                  : ""
+                : changeFont === "italic"
+                ? "txt-italic"
+                : ""
+            }
             className={char.length === 120 ? "d-write" : ""}
           />
         </GrammarlyEditorPlugin>
